e0ead5cd5b36da0dab323454e66aa6c060f87d1e
celomamp <marcelo.mpinheiro96@gmail.com>
2016-01-18 11:36:54 -0200

    Primeiro Commit: Instanciação da classe nfePHP

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Transmissão NFE FIT4</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- JQUERY -->
+		<script type="text/javascript" src="assets/js/jquery-2.1.4.js"></script>
+
+		<!-- OPEN -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&subset=latin,latin-ext" rel="stylesheet" type="text/css">
+
+		<!-- BOOTSTRAP -->
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.css">
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+		<!-- DEFAULT -->
+		<link rel="stylesheet" type="text/css" href="assets/css/default.css">
+		<script type="text/javascript" src="assets/js/default.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12">
+					<h1>Transmissão Nota Fiscal Eletrônica - FIT4</h1>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
