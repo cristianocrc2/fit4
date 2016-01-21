d93ed3aa4a93b691cb85505d1179f9bfb87d629a
Marcelo Pinheiro <Marcelo Pinheiro>
2016-01-21 11:11:12 -0200

    Conclusão de testes de homologação

--- controller.php
+++ controller.php
@@ -17,19 +17,46 @@
 		    
 			$nfe = new ToolsNFe('C:/xampp/htdocs/cristiano/fit4/nfe/config.json');
 			$nfe->setModelo('55');
+			
 
-			$aResposta = array();
-			$tpAmb = '2';
-			$aXml = file_get_contents($uploadfile);
+			// ========================= TRANSMISSÃO SEFAZ =====================
+
+			$tpAmb = '2'; //tipo de ambiente (Homologação)
+			$aResposta = array(); // Instancia um array de respostas			
+			$aXml = file_get_contents($uploadfile); //Pega o arquivo xml
 			$idLote = '';
 			$indSinc = '0';
 			$flagZip = false;
-			$retorno = $nfe->sefazEnviaLote($aXml, $tpAmb, $idLote, $aResposta, $indSinc, $flagZip);
-			echo '<br><br><PRE>';
-			echo htmlspecialchars($nfe->soapDebug);
-			echo '</PRE><BR>';
-			print_r($aResposta);
-			echo "<br>";
+			$retorno = $nfe->sefazEnviaLote($aXml, $tpAmb, $idLote, $aResposta, $indSinc, $flagZip); //Se comunica com a sefaz
+			$numero_recibo = $aResposta['nRec']; //Recebe o numero do recibo
+
+			//========================== CONSULTA RETORNO (RECIBO) ==============
+
+			$tpAmb = '2'; //tipo de ambiente (Homologação)
+			$aRespostaRecibo = array();
+			$recibo = $numero_recibo;
+			$retorno = $nfe->sefazConsultaRecibo($recibo, $tpAmb, $aRespostaRecibo); //envia o recibo para receber protocolo
+			$protocolo = $aRespostaRecibo['nRec']; //recebe numero do protocolo
+
+			//============================ ADICIONA PROTOCOLO AO XML ============== 
+			
+			$aRespostaProtocolo = array();
+
+			$pathNFefile = $uploadfile; //Caminho para o xml
+			$pathProtfile = 'C:/xampp/htdocs/cristiano/fit4/enviadas/homologacao/temporarias/'.date("Ym").'/'.$protocolo.'-retConsReciNFe.xml'; //Caminho para o protocolo
+			$saveFile = true;
+			$retorno = $nfe->addProtocolo($pathNFefile, $pathProtfile, $saveFile); //Adiciona o protocolo a xml
+			$string = $retorno; //Recebe o xml protocolado
+
+			header('Content-type: text/plain');
+			header('Content-Length: ' . strlen( $string ) ); 													//EFETUA O DOWNLOAD
+			header('Content-Disposition: attachment; filename="NFe-FIT4-'.md5(uniqid(rand(), true)).'.xml"');
+
+			echo $string;
+	
+			
+
+
 
 
 		} else {
