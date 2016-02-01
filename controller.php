122469dde12fa2e314d2bf96cce739e1a0ef20bf
Marcelo Pinheiro <Marcelo Pinheiro>
2016-02-01 14:32:04 -0200

    Desenvolvimento formulário inutilização

--- controller.php
+++ controller.php
@@ -6,60 +6,71 @@
 	use NFePHP\NFe\ToolsNFe;
 	
 	if(isset($_POST['password']) && $_POST['password'] == "senha"){ //Se estiver setada e correta a senha de upload
-		$uploaddir = 'C:\\xampp\\htdocs\\cristiano\\fit4\\nfe\\enviadas'; //Instancia variavel com o destino do arquivo
-		if(!is_dir($uploaddir)){ //Se não existe
-			mkdir($uploaddir); //Cria o destino
+
+
+ ############################################################# TRANSMISSÃO NFE ##########################################################
+		if(isset($_POST['function']) && $_POST['function'] == "transmissao"){ //SE FOR TRANSMISSÃO DE NOTA FISCAL
+			$uploaddir = 'C:\\xampp\\htdocs\\cristiano\\fit4\\nfe\\enviadas'; //Instancia variavel com o destino do arquivo
+			if(!is_dir($uploaddir)){ //Se não existe
+				mkdir($uploaddir); //Cria o destino
+			}
+
+			$uploadfile = $uploaddir ."\\". basename($_FILES['xml']['name']); //Contrói o namespace completo do file
+
+			if (move_uploaded_file($_FILES['xml']['tmp_name'], $uploadfile)) { //Se conclui o upload
+			    
+				$nfe = new ToolsNFe('C:/xampp/htdocs/cristiano/fit4/nfe/config.json');
+				$nfe->setModelo('55');
+				
+
+				// ========================= TRANSMISSÃO SEFAZ =====================
+
+				$tpAmb = '2'; //tipo de ambiente (Homologação)
+				$aResposta = array(); // Instancia um array de respostas			
+				$aXml = file_get_contents($uploadfile); //Pega o arquivo xml
+				$idLote = '';
+				$indSinc = '0';
+				$flagZip = false;
+				$retorno = $nfe->sefazEnviaLote($aXml, $tpAmb, $idLote, $aResposta, $indSinc, $flagZip); //Se comunica com a sefaz
+				$numero_recibo = $aResposta['nRec']; //Recebe o numero do recibo
+
+				//========================== CONSULTA RETORNO (RECIBO) ==============
+
+				$tpAmb = '2'; //tipo de ambiente (Homologação)
+				$aRespostaRecibo = array();
+				$recibo = $numero_recibo;
+				$retorno = $nfe->sefazConsultaRecibo($recibo, $tpAmb, $aRespostaRecibo); //envia o recibo para receber protocolo
+				$protocolo = $aRespostaRecibo['nRec']; //recebe numero do protocolo
+
+				//============================ ADICIONA PROTOCOLO AO XML ============== 
+				
+				$aRespostaProtocolo = array();
+
+				$pathNFefile = $uploadfile; //Caminho para o xml
+				$pathProtfile = 'C:/xampp/htdocs/cristiano/fit4/enviadas/homologacao/temporarias/'.date("Ym").'/'.$protocolo.'-retConsReciNFe.xml'; //Caminho para o protocolo
+				$saveFile = true;
+				$retorno = $nfe->addProtocolo($pathNFefile, $pathProtfile, $saveFile); //Adiciona o protocolo a xml
+				$string = $retorno; //Recebe o xml protocolado
+
+				header('Content-type: text/plain');
+				header('Content-Length: ' . strlen( $string ) ); 													//EFETUA O DOWNLOAD
+				header('Content-Disposition: attachment; filename="NFe-FIT4-'.md5(uniqid(rand(), true)).'.xml"');
+
+				echo $string;
+			}
+		}
+######################################### FIM TRANSMISSÃO #########################################################################################
+
+##################################################### INUTILIZAÇÃO ##############################################################################333
+		if(isset($_POST['function']) && $_POST['function'] == "inutilizacao"){ //SE FOR inutilização DE NOTA FISCAL
+
+			echo "inutilização";
 		}
 
-		$uploadfile = $uploaddir ."\\". basename($_FILES['xml']['name']); //Contrói o namespace completo do file
+##########################################3# FIM INUTILIZAÇÃO ###############################################################################
 
-		if (move_uploaded_file($_FILES['xml']['tmp_name'], $uploadfile)) { //Se conclui o upload
-		    
-			$nfe = new ToolsNFe('C:/xampp/htdocs/cristiano/fit4/nfe/config.json');
-			$nfe->setModelo('55');
-			
-
-			// ========================= TRANSMISSÃO SEFAZ =====================
-
-			$tpAmb = '2'; //tipo de ambiente (Homologação)
-			$aResposta = array(); // Instancia um array de respostas			
-			$aXml = file_get_contents($uploadfile); //Pega o arquivo xml
-			$idLote = '';
-			$indSinc = '0';
-			$flagZip = false;
-			$retorno = $nfe->sefazEnviaLote($aXml, $tpAmb, $idLote, $aResposta, $indSinc, $flagZip); //Se comunica com a sefaz
-			$numero_recibo = $aResposta['nRec']; //Recebe o numero do recibo
-
-			//========================== CONSULTA RETORNO (RECIBO) ==============
-
-			$tpAmb = '2'; //tipo de ambiente (Homologação)
-			$aRespostaRecibo = array();
-			$recibo = $numero_recibo;
-			$retorno = $nfe->sefazConsultaRecibo($recibo, $tpAmb, $aRespostaRecibo); //envia o recibo para receber protocolo
-			$protocolo = $aRespostaRecibo['nRec']; //recebe numero do protocolo
-
-			//============================ ADICIONA PROTOCOLO AO XML ============== 
-			
-			$aRespostaProtocolo = array();
-
-			$pathNFefile = $uploadfile; //Caminho para o xml
-			$pathProtfile = 'C:/xampp/htdocs/cristiano/fit4/enviadas/homologacao/temporarias/'.date("Ym").'/'.$protocolo.'-retConsReciNFe.xml'; //Caminho para o protocolo
-			$saveFile = true;
-			$retorno = $nfe->addProtocolo($pathNFefile, $pathProtfile, $saveFile); //Adiciona o protocolo a xml
-			$string = $retorno; //Recebe o xml protocolado
-
-			header('Content-type: text/plain');
-			header('Content-Length: ' . strlen( $string ) ); 													//EFETUA O DOWNLOAD
-			header('Content-Disposition: attachment; filename="NFe-FIT4-'.md5(uniqid(rand(), true)).'.xml"');
-
-			echo $string;
-	
-			
-
-
-
-
-		} else {
+	} else {
 		    echo "Possível ataque de upload de arquivo!\n";
-		}
 	}
+
+	
--- index.php
+++ index.php
@@ -23,19 +23,67 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 col-md-12">
-					<h1>Transmissão Nota Fiscal Eletrônica - FIT4</h1>
+					<h1>Nota Fiscal Eletrônica - FIT4</h1>
+				</div>
+				<div class="col-lg-6 col-md-6 panel panel-default">
+				<h3>Transmissão</h3>
 					<div class="upload_div">
 						<form method="post" action="controller.php" enctype="multipart/form-data">
 							<div class="form-group">
 								<label for="file">XML File</label>
 								<input type="hidden" name="password" value="senha">
+								<input type="hidden" name="function" value="transmissao">
 								<input type="file" name="xml" id="xml"><br />
 								<input type="submit" name="submit" value="Enviar Nota" class="btn btn-default">
 							</div>
 						</form>						
-					</div>
-				</div>
-			</div>
-		</div>
+					</div> <!-- UPLOAD DIV -->
+				</div>	<!-- COL-LG-6 COL-MD-6 -->	
+				<div class="col-lg-6 col-md-6 panel panel-default">
+					<h3>Pedido de inutilização</h3>
+					<form method="post" action="controller.php">
+					<input type="hidden" name="function" value="inutilizacao">
+					<input type="hidden" name="password" value="senha">
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<div class="form-group">
+									<label for="ano">Ano</label>
+									<input type="text" id="ano" name="ano" class="form-control">
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-3">
+								<div class="form-group">
+									<label for="serie">Série</label>
+									<input type="text" id="serie" name="serie" value="1" class="form-control">
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-3">
+								<div class="form-group">
+									<label for="ininmr">Número Inicial</label>
+									<input type="text" id="ininmr" name="ininmr" class="form-control">
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-3">
+								<div class="form-group">
+									<label for="fimnmr">Número Final</label>
+									<input type="text" id="fimnmr" name="fimnmr" class="form-control">
+								</div>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="form-group">
+									<label for="justificativa">Justificativa</label>
+									<textarea id="justificativa" name="justificativa" class="form-control"></textarea>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-3">
+								<div class="form-group">
+									<input type="submit" value="Enviar Pedido" class="btn btn-warning" style="margin-top: 20px;">
+								</div>
+							</div>
+						</div>
+					</form>
+				</div> <!-- COL-LG-6 COL-MD-6 -->		
+			</div> <!-- ROW -->
+		</div> <!-- CONTAINER -->
 	</body>
 </html>
